3747b1da62c7a6f75e5e4d2590d65a4e48dc5f83
tatiana brito <brito.tatiana@gmail.com>
2020-11-13 19:58:26 -0300

    UserRepository, UserResource, Zuul Config

---
+++ hr-user/src/main/java/com/tatianabrito/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tatianabrito.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tatianabrito.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+}
